8cc708e3bc28df902bd9a895e6e6e98db4ac7388
Alexandre Salomé <git@pub.salome.fr>
2021-04-05 20:04:14 +0200

    Bootstrap tuning

--- assets/app.ts
+++ assets/app.ts
@@ -1,5 +1,5 @@
-// any CSS you import will output into a single css file (app.css in this case)
+import 'bootstrap/js/src/dropdown';
+
 import './styles/app.sass';
 
-// start the Stimulus application
 import './bootstrap';
