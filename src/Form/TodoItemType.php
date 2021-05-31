7c114500b51dddb3a371995aa6e7266925ef1dcd
Alexandre Salomé <git@pub.salome.fr>
2021-05-31 09:14:20 +0200

    Hide position field

--- src/Form/TodoItemType.php
+++ src/Form/TodoItemType.php
@@ -4,7 +4,7 @@ namespace App\Form;
 
 use App\Entity\TodoItem;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,7 +14,7 @@ class TodoItemType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('position', IntegerType::class)
+            ->add('position', HiddenType::class)
             ->add('title', TextType::class)
         ;
     }
